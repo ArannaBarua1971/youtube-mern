a6f10a07c66361e6cc6e14207223357953112fe0
ArannaBarua1971 <arannabaruase71@gmail.com>
2025-10-11 12:18:37 +0600

    add video endpoint

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { validedAuth } from "../middlewares/auth.middleware.js";
 import {
+  deleteVideo,
   getAllVideos,
   getVideoById,
   publishAVideo,
+  togglePublishStatus,
+  updateVideo,
 } from "../controllers/video.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
@@ -14,6 +17,9 @@ router.use(validedAuth);
 //secure video router
 router.route("/v/:id").get(getVideoById);
 router.route("/all").get(getAllVideos);
+router.route("/updateVideo/:id").patch(updateVideo);
+router.route("/deleteVideo/:id").patch(deleteVideo);
+router.route("/toggleStatus/:id").patch(togglePublishStatus);
 router.route("/publishVideo").post(
   upload.fields([
     { name: "videoFile", maxCount: 1 },
